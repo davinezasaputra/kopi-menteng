49e02f6c73d92abd209048d11077287f8422649f
Davin Eza Saputra <davin.eza2004@gmail.com>
2026-08-31 07:22:24 +0700

    feat: register ERP middleware aliases

--- pos-menteng-backend/bootstrap/app.php
+++ pos-menteng-backend/bootstrap/app.php
@@ -1,5 +1,8 @@
 <?php
 
+use App\Http\Middleware\AddRequestId;
+use App\Http\Middleware\RequirePermission;
+use App\Http\Middleware\ResolveTenantContext;
 use Illuminate\Foundation\Application;
 use Illuminate\Foundation\Configuration\Exceptions;
 use Illuminate\Foundation\Configuration\Middleware;
@@ -12,7 +15,11 @@ return Application::configure(basePath: dirname(__DIR__))
         health: '/up',
     )
     ->withMiddleware(function (Middleware $middleware): void {
-        //
+        $middleware->alias([
+            'request.id' => AddRequestId::class,
+            'tenant' => ResolveTenantContext::class,
+            'permission' => RequirePermission::class,
+        ]);
     })
     ->withExceptions(function (Exceptions $exceptions): void {
         //
