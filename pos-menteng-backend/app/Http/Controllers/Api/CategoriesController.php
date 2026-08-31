9412ae52d9362f051f98d55f624baf27a50177de
Davin Eza Saputra <davin.eza2004@gmail.com>
2026-08-31 09:33:03 +0700

    fix: restore categories controller namespace compatibility

---
+++ pos-menteng-backend/app/Http/Controllers/Api/CategoriesController.php
@@ -0,0 +1,8 @@
+<?php
+
+namespace App\Http\Controllers\Api;
+
+class CategoriesController extends \App\Http\Controllers\CategoriesController
+{
+    // Compatibility adapter for the existing route namespace.
+}
