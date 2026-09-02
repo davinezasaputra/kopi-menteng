d1cbd7269cd8cbd4f11ff04797034c07767dc29c
Davin Eza Saputra <davin.eza2004@gmail.com>
2026-09-02 10:59:33 +0700

    chore(seed): keep only developer account in base seeder

--- pos-menteng-backend/database/seeders/DatabaseSeeder.php
+++ pos-menteng-backend/database/seeders/DatabaseSeeder.php
@@ -13,9 +13,6 @@ class DatabaseSeeder extends Seeder
         User::updateOrCreate(['email'=>'davin-eza@mahasiswa.ubb.ac.id'],[
             'name'=>'Davin (Developer)','password'=>Hash::make(env('SEED_DEVELOPER_PASSWORD','change-me-immediately')),'role'=>'developer','pin'=>null,
         ]);
-        User::updateOrCreate(['email'=>'kasir1@menteng.com'],[
-            'name'=>'Kasir Satu','password'=>Hash::make(env('SEED_CASHIER_PASSWORD','change-me-immediately')),'role'=>'kasir','pin'=>env('SEED_CASHIER_PIN'),
-        ]);
         $this->call([
             ErpMasterSeeder::class,
             OperationalExpenseAccountSeeder::class,
